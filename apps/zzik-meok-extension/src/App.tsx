8f737524bc82ea3a6ad2dae7a3cb423a02e98169
김명성 <maeng2418@naver.com>
2024-12-26 17:43:34 +0900

    feat: Extension manifest 작성

--- apps/zzik-meok-extension/src/App.tsx
+++ apps/zzik-meok-extension/src/App.tsx
@@ -1,7 +1,5 @@
 import { useState } from 'react'
 import './App.css'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,10 +8,10 @@ function App() {
     <>
       <div>
         <a href="https://vite.dev" target="_blank" rel="noreferrer noopener">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          Vite logo
         </a>
         <a href="https://react.dev" target="_blank" rel="noreferrer noopener">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          logo react
         </a>
       </div>
       <h1>Vite + React</h1>

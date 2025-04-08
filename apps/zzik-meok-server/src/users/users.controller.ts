7bb4296ca95398ab3dfd03afe26bbfd53c13ff08
김명성 <maeng2418@naver.com>
2025-04-08 23:32:20 +0900

    feat: usersController 재작성

--- apps/zzik-meok-server/src/users/users.controller.ts
+++ apps/zzik-meok-server/src/users/users.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common'
-import { UsersService } from './users.service'
+import { Controller, Post, Body, Param, Get, BadRequestException } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { userSchema } from './user.schema';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Get()
-  getHello() {
-    return this.usersService.getHello()
+  @Post()
+  async createUser(@Body() createUserDto: { name: string; email: string; password: string }) {
+    const { error } = userSchema.validate(createUserDto);
+    if (error) {
+      throw new BadRequestException(error.details[0].message);
+    }
+    return this.usersService.createUser(createUserDto.name, createUserDto.email, createUserDto.password);
+  }
+
+  @Post(':id/urls')
+  async addUrl(@Param('id') id: number, @Body('url') url: string) {
+    return this.usersService.addUrlToUser(id, url);
+  }
+
+  @Get(':id/urls')
+  async getUserUrls(@Param('id') id: number) {
+    return this.usersService.getUserUrls(id);
   }
 }

d1d39d41c98abc0dc2b36ac85c1d2200eb4cdba9
김명성 <maeng2418@naver.com>
2025-04-08 23:11:22 +0900

    feat: UsersService에 createUser, findOne 메서드 추가

--- apps/zzik-meok-server/src/users/users.service.ts
+++ apps/zzik-meok-server/src/users/users.service.ts
@@ -1,8 +1,29 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  async createUser(name: string, email: string, password: string): Promise<User> {
+    const user = this.usersRepository.create({ name, email, password });
+    return this.usersRepository.save(user);
+  }
+
+  async findOne(id: number): Promise<User> {
+    const user = await this.usersRepository.findOneBy({ id });
+    if (!user) {
+      throw new NotFoundException(`User with ID ${id} not found`);
+    }
+    return user;
+  }
+
   getHello() {
-    return { data: { message: 'Hello World!' } }
+    return { data: { message: 'Hello World!' } };
   }
 }

3c8822432502613bbd565d95e17776a8432bd5c3
김명성 <maeng2418@naver.com>
2025-04-08 23:22:27 +0900

    feat: TypeORM 설정 및 AppModule에 UrlsModule 등록

--- apps/zzik-meok-server/src/app.module.ts
+++ apps/zzik-meok-server/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { validationSchema } from './config/validationSchema'
 import { UsersModule } from './users/users.module'
+import { UrlsModule } from './urls/urls.module'
+import { User } from './users/user.entity'
+import { Url } from './urls/url.entity'
 
 @Module({
   imports: [
@@ -12,7 +16,14 @@ import { UsersModule } from './users/users.module'
       isGlobal: true,
       validationSchema,
     }),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'zzik-meok.sqlite',
+      entities: [User, Url],
+      synchronize: true,
+    }),
     UsersModule,
+    UrlsModule,
   ],
   controllers: [AppController],
   providers: [AppService],

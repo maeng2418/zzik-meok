b14e9a48773f25c25e07afedfd47a2a5d5c646d9
김명성 <maeng2418@naver.com>
2025-04-08 23:08:28 +0900

    feat: Url 엔티티 검증을 위한 Joi 스키마 추가

---
+++ apps/zzik-meok-server/src/urls/url.schema.ts
@@ -0,0 +1,13 @@
+import * as Joi from 'joi';
+import { userSchema } from '../users/user.schema';
+
+export const urlSchema = Joi.object({
+  url: Joi.string().uri().required(),
+  title: Joi.string().allow('', null),
+  description: Joi.string().allow('', null),
+  isBookmarked: Joi.boolean().default(false),
+  createdAt: Joi.date(),
+  updatedAt: Joi.date(),
+  userId: Joi.number().integer().positive().required(),
+  user: userSchema
+});
